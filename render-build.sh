c3b155de3de37b0a9a1af97a99ea034700259509
tanushigarg <tanushidata1@gmail.com>
2025-03-21 09:27:46 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-# Install Chrome
+# Install dependencies
+apt-get update && apt-get install -y unzip wget curl
+
+# Install Google Chrome
 echo "Installing Google Chrome..."
 wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt-get update
 apt-get install -y ./google-chrome-stable_current_amd64.deb
 
-# Install ChromeDriver
+# Install ChromeDriver (matching Chrome version)
 echo "Installing ChromeDriver..."
 CHROME_VERSION=$(google-chrome --version | awk '{print $3}' | cut -d '.' -f 1)
 wget -q "https://chromedriver.storage.googleapis.com/${CHROME_VERSION}.0/chromedriver_linux64.zip"
 unzip chromedriver_linux64.zip
-mv chromedriver /usr/local/bin/chromedriver
-chmod +x /usr/local/bin/chromedriver
+mv chromedriver /usr/bin/chromedriver
+chmod +x /usr/bin/chromedriver
 
 # Clean up
 rm -f google-chrome-stable_current_amd64.deb chromedriver_linux64.zip
